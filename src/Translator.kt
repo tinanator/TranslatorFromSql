57ac872185163d8521fea616d3577f619992c67d
Tina <kviditch@gmail.com>
2020-04-05 03:04:25 +1000

    add private methods, some fix

--- src/Translator.kt
+++ src/Translator.kt
@@ -1,73 +1,88 @@
 enum class State {
     SELECT, ALL, NAME, FROM, NAME_COMMA, NONE
 }
-class Traslator {
 
-    var prevState = 0
-    var matrix = arrayOf<Array<Int>>(arrayOf(1, -1, -1, -1, -1),
-                                     arrayOf(-1, 2, 3, 4, -1),
-                                     arrayOf(-1, -1, -1, -1, 5),
-                                     arrayOf(-1, -1, 3, 4, -1),
-                                     arrayOf(-1, -1, -1, -1, 5),
-                                     arrayOf(-1, -1, -1, 6, -1))
+
+class Traslator {
+    private var mongodb : String = String()
+    private var prevState = 0
+    private var command = String()
+    private var tbName = String()
+    private var isError = false
+    private val columns : MutableList<String> = mutableListOf()
+    private var matrix = arrayOf<Array<Int>>(arrayOf(1, -1, -1, -1, -1),
+                                             arrayOf(-1, 2, 3, 4, -1),
+                                             arrayOf(-1, -1, -1, -1, 5),
+                                             arrayOf(-1, -1, 3, 4, -1),
+                                             arrayOf(-1, -1, -1, -1, 5),
+                                             arrayOf(-1, -1, -1, 6, -1))
+
+    private fun defineLexem(word : String) : State {
+        val lexem = if (word[word.length - 1] == ',') {
+            State.NAME_COMMA
+        } else {
+            when(word) {
+                "SELECT" -> { State.SELECT }
+                "*" -> { State.ALL }
+                "FROM" -> { State.FROM }
+                else -> { State.NAME }
+            }
+        }
+        return lexem
+    }
+
+    private fun setState(lexem : State) : Int {
+        var curState = -1
+        when(lexem) {
+            State.NAME_COMMA -> {curState = 2}
+            State.ALL -> { curState = 1 }
+            State.SELECT -> {curState = 0 }
+            State.NAME -> {curState = 3}
+            State.FROM -> {curState = 4}
+            else -> {}
+        }
+        return curState
+    }
+
+    private fun translateToMongodb(curState:Int, lexem:String){
+        when(curState) {
+            -1 -> {
+                isError = true
+                //return mongodb
+            }
+            1 -> {
+                command = "find"
+            }
+            3 -> {
+                columns.add(lexem.substring(0, lexem.length - 1))
+                //govno = lexem.substring(0, lexem.length - 1)
+                println(lexem.substring(0, lexem.length - 1))
+            }
+            4 -> {
+                columns.add(lexem)
+            }
+            6 -> {
+                tbName = lexem
+            }
+        }
+    }
 
     fun translate(sqlCommand : Array<String>) : String {
-
-        var mongodb : String = String()
         val tmp : String = String()
-        var tbName = String()
         var cols = String()
-
-        val columns : MutableList<String> = mutableListOf()
         var curState = 0
-        var command = String()
-        var name : State = State.NONE
+        var lexem : State = State.NONE
         for (word in sqlCommand) {
-            name = if (word[word.length - 1] == ',') {
-                State.NAME_COMMA
-            } else {
-                when(word) {
-                    "SELECT" -> { State.SELECT }
-                    "*" -> { State.ALL }
-                    "FROM" -> { State.FROM }
-                    else -> { State.NAME }
-                }
-            }
-
-            when(name) {
-                State.NAME_COMMA -> {curState = 2}
-                State.ALL -> { curState = 1 }
-                State.SELECT -> {curState = 0 }
-                State.NAME -> {curState = 3}
-                State.FROM -> {curState = 4}
-                else -> {}
-            }
-
+            lexem = defineLexem(word)
+            curState = setState(lexem)
             curState =  matrix[prevState][curState]
             prevState = curState
             var govno = String()
-            when(curState) {
-                -1 -> {
-                    mongodb = "error"
-                    return mongodb
-                }
-                1 -> {
-                    command = "find"
-                }
-                3 -> {
-                    columns.add(word.dropLast(2))
-                    govno = word.substring(0, word.length - 1)
-                    println(word)
-                }
-                4 -> {
-                    columns.add(word)
-                }
-                6 -> {
-                    tbName = word
-                }
+            translateToMongodb(curState, word)
+            if (isError) {
+                return "ERROR"
             }
 
-            //columns.clear()
 
         }
         for (col in columns) {
@@ -82,8 +97,6 @@ class Traslator {
             cols.dropLast(cols.length - 1)
             t = (cols.subSequence(0, cols.length - 2)).toString()
         }
-
-
         mongodb = "db.$tbName.$command({}$t)"
         return mongodb
     }
