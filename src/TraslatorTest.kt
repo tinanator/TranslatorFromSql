9ed01758011e59b700ac9b4982a0460fadcc84fa
Tina <kviditch@gmail.com>
2020-04-06 03:25:13 +1000

    ghjkl

--- src/TraslatorTest.kt
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Assertions.*
-
-internal class TraslatorTest {
-    @org.junit.jupiter.api.Test
-
-    fun testTranlate() {
-        var sqlString = "SELECT * FROM TABLE T"
-    }
-}
